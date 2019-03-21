ec30c761fa9f7b7acc6f1eef4d12a288a0eb7bf1
Alder9 <thomas.alder@colorado.edu>
2019-03-21 17:07:47 -0600

    Changes to algorithm to load data to local SQL server

--- Data-warehouse/prepare_data.py
+++ Data-warehouse/prepare_data.py
@@ -1,36 +1,47 @@
 import pandas as pd
 import numpy as np
 from sqlalchemy import create_engine
+import pymysql
 
 
 def create_table_from_csv(engine):
 	print('Loading data set')
 	# Reading the csv and putting into dataframe
 	# note: change to dir. where your data set is
-	dfKills = pd.read_csv('F:\\Datasets\\pubg-match-deaths\\deaths\\kill_match_stats_final_0.csv')
-	dfAgg = pd.read_csv('F:\\Datasets\\pubg-match-deaths\\aggregate\\agg_match_stats_0.csv')
-	print('Data set loaded')
-	print(dfKills.head())
-	print('-------------------')
-	print(dfAgg.head())
+	chunk_count = 0
+	for chunk in pd.read_csv('F:\\Datasets\\pubg-match-deaths\\deaths\\kill_match_stats_final_0.csv', chunksize=10):
+		chunk.to_sql('deaths', con=engine, if_exists='append', index=False)
+		chunk_count += 10
+		# print(chunk.iloc[0,0])
+		print('{} rows loaded'.format(chunk_count))
+
+	# chunk_count = 0
+	# for chunk in pd.read_csv('F:\\Datasets\\pubg-match-deaths\\aggregate\\agg_match_stats_0.csv', chunksize=10):
+	# 	chunk.to_sql('aggregate', con=engine, if_exists='append', index=False)
+	# 	chunk_count += 10
+	# 	print(chunk.iloc[0,0])
+	# 	print('{} rows loaded'.format(chunk_count))
+
+	# print('Data set loaded')
+	# print(dfKills.head())
+	# print('-------------------')
+	# print(dfAgg.head())
 
 	# Turn dataframes to SQL tables and insert into MySQL connection
 	# If the table exists, for the time being, replace it
-	dfAgg.to_sql('player', con=engine, if_exists='replace')
-	dfKills.to_sql('death', con=engine, if_exists='replace')
-	print('Sql tables created')
+	print('Sql table created')
 	
 
 def main():
 	# Connecting to local sql server for backend - this will be the store 
 	# for the data warehouse
+
 	print('Creating engine') 
 	# MySQL server on localhost:3306 with user admin
-	engine = create_engine('mysql://admin:admin123@localhost:3306/pubg')
+	engine = create_engine('mysql+pymysql://admin:admin123@localhost:3306/pubg')
 	print('Engine created')
 
-	# create_table_from_csv(engine)
-	# aggregate()
+	create_table_from_csv(engine)
 
 if __name__ == '__main__':
 	main()
