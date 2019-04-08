6795c96e20e32ebfae7c361ab3c0c55447031c93
Alder9 <thomas.alder@colorado.edu>
2019-04-08 16:26:59 -0600

    Added backend mysql model and picture of schema

--- Data-warehouse/prepare_data.py
+++ Data-warehouse/prepare_data.py
@@ -8,19 +8,19 @@ def create_table_from_csv(engine):
 	print('Loading data set')
 	# Reading the csv and putting into dataframe
 	# note: change to dir. where your data set is
-	chunk_count = 0
-	for chunk in pd.read_csv('F:\\Datasets\\pubg-match-deaths\\deaths\\kill_match_stats_final_0.csv', chunksize=10):
-		chunk.to_sql('deaths', con=engine, if_exists='append', index=False)
-		chunk_count += 10
-		# print(chunk.iloc[0,0])
-		print('{} rows loaded'.format(chunk_count))
-
 	# chunk_count = 0
-	# for chunk in pd.read_csv('F:\\Datasets\\pubg-match-deaths\\aggregate\\agg_match_stats_0.csv', chunksize=10):
-	# 	chunk.to_sql('aggregate', con=engine, if_exists='append', index=False)
+	# for chunk in pd.read_csv('F:\\Datasets\\pubg-match-deaths\\deaths\\kill_match_stats_final_0.csv', chunksize=10):
+	# 	chunk.to_sql('deaths', con=engine, if_exists='append', index=False)
 	# 	chunk_count += 10
-	# 	print(chunk.iloc[0,0])
+	# 	# print(chunk.iloc[0,0])
 	# 	print('{} rows loaded'.format(chunk_count))
+
+	chunk_count = 0
+	for chunk in pd.read_csv('F:\\Datasets\\pubg-match-deaths\\aggregate\\agg_match_stats_0.csv', chunksize=100):
+		chunk.to_sql('aggregate', con=engine, if_exists='append', index=False)
+		chunk_count += 100
+		print(chunk.iloc[0,0])
+		print('{} rows loaded'.format(chunk_count))
 
 	# print('Data set loaded')
 	# print(dfKills.head())
