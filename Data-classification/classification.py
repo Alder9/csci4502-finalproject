c3ce068129cc706ab627ac5dae0ba2aa739bfbb1
Matthew Davenport <mada6251@colorado.edu>
2019-04-27 21:02:47 -0600

    Update classification.py

--- Data-classification/classification.py
+++ Data-classification/classification.py
@@ -1,36 +1,27 @@
 import numpy as np
 import pandas as pd
-from matplotlib import pyplot as plt
 from sklearn.naive_bayes import GaussianNB, BernoulliNB, MultinomialNB
-
 
 # Import first two csvs as training set
 
 path = r'C:/Users/matt-/Desktop/Data Mining/pubg-match-deaths/aggregate/'
 
-# Training data will be agg_match_stats_0 to 2
-# Testing data will be agg_match_stats_3 and 4
-#training_paths = [path + r'agg_match_stats_' + str(i) + '.csv' for i in range(0,2)]
-# testing_path = [path + r'agg_match_stats_' + str(i) + '.csv' for i in range(3,4)]
+# Training data will be agg_match_stats_0
+# Testing data will be agg_match_stats_3
+
 
 print("Loading dataframe")
 training_dfs = []
 testing_dfs = []
-# for i in range(0,len(training_paths)):
-#     df = pd.read_csv(training_paths[i], index_col=None, header=0)
-#     training_dfs.append(df)
 
-    # if(i < len(testing_path)):
-    #     df2 = pd.read_csv(testing_path[i], index_col=None, header=0)
-    #     testing_dfs.append(df)
 
 train_data = pd.read_csv(path + r'agg_match_stats_0.csv', index_col=None, header=0)
 test_data = pd.read_csv(path + r'agg_match_stats_3.csv', index_col=None, header=0)
 
 print("Datasets loaded, starting cleaning")
 
-train_data["team_placement_cleaned"]=np.where(train_data["team_placement"]==1,2,1)
-test_data["team_placement_cleaned"]=np.where(test_data["team_placement"]==1,2,1)
+train_data["team_placement_cleaned"]=np.where(train_data["team_placement"]==1, 0, 1)
+test_data["team_placement_cleaned"]=np.where(test_data["team_placement"]==1, 0, 1)
 
 train_data=train_data[[
     "party_size",
